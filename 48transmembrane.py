56a3ebf1f34724c248108e8127cd3204d508ec6a
kassidyyu <kcyu@ucdavis.edu>
2025-07-02 00:20:33 -0700

    format

--- 48transmembrane.py
+++ 48transmembrane.py
@@ -13,32 +13,32 @@ KD = {
 
 for defline, protein in mcb185.read_fasta(sys.argv[1]):
 	if len(protein) < 41: continue
-	subseq = protein[:8]                   # initialize first potential signal
+	subseq = protein[:8] # initialize first potential signal
 	signal = False
 	# 8 long so look through first 22 for signal peptide in first 30 aa's
 	for i in range(23): 
 		if 'P' in subseq:                  # no proline in signal peptides
-			if i == 22: break              # last loop, iteration will fail
+			if i == 22: break   		   # last loop, iteration will fail
 			subseq = subseq[1:] + protein[i + 8]
-			continue                       # skip calculation
+			continue            	       # skip calculation
 		kd_sum = 0                         # initialize/reset sum to 0
 		for aa in subseq: kd_sum += KD[aa] # sum scores
 		if kd_sum / 8 >= 2.5:              # found signal peptide
 			signal = True
-			break                          # stop looping
+			break           	           # stop looping
 		if i < 22: subseq = subseq[1:] + protein[i + 8]
-	if not signal: continue                # no signal, move onto next protein
+	if not signal: continue # no signal, move onto next protein
 	# repeat similar process to look for transmembrane regions
 	region = protein[30:41]
 	tm_reg = False
 	for i in range(30, len(protein) - 10): # windows of 11, look through rest
-		if 'P' in region:                  # no proline
+		if 'P' in region:           	   # no proline
 			if i == len(protein) - 11: break
 			region = region[1:] + protein[i + 11]
 			continue
 		kd_sum = 0
 		for aa in region: kd_sum += KD[aa]
-		if kd_sum / 11 >= 2.0:             # found transmembrane region
+		if kd_sum / 11 >= 2.0:  	       # found transmembrane region
 			tm_reg = True
 			break
 		if i < len(protein) - 11: region = region[1:] + protein[i + 11]
